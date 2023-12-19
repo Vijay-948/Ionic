a2186ab6c0d0782809ab04e12922f62c0fd40eea
Kogilathota Vijay Reddy <vijay.kogilathota@peopletech.com>
2023-12-19 18:11:10 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+import "@ionic/react/css/core.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Name");
+  const [birthDate, setBirthDate] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Hello World</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Name:</IonLabel>
+            <IonInput
+              placeholder="Enter Name"
+              value={name}
+              onIonChange={(event) => setName(event.detail.value)}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Date Of Birth:</IonLabel>
+            <IonInput
+              type="date"
+              value={birthDate}
+              onIonChange={(event) => setBirthDate(event.detail.value)}
+            />
+          </IonItem>
+        </IonList>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your Name: <b>{name} </b>, Born on: <b>{birthDate}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </IonContent>
+    </IonApp>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { setupIonicReact } from "@ionic/react";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+setupIonicReact();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
